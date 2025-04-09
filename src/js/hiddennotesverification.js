1c56d7cff7fd38f719941402b2526a2162a62c0b
ayberkozcan <ozcanayberk249@gmail.com>
2025-04-09 20:11:18 +0300

    db connection

--- src/js/hiddennotesverification.js
+++ src/js/hiddennotesverification.js
@@ -2,17 +2,48 @@ const form = document.getElementById("form");
 const password = document.getElementById("password");
 const verificationStatus = document.getElementById("verificationStatus");
 
+// function verifyInput(input) {
+//     if (input.value == "asd1234") {
+//         console.log("Verification Successful");
+
+//         localStorage.setItem("isVerified", "true");
+
+//         window.location.href = "homepage.html";
+//     } else {
+//         console.log("Verification Unsuccessful");
+//         error("Wrong Password!");
+//     }
+// }
+
 function verifyInput(input) {
-    if (input.value == "asd1234") {
-        console.log("Verification Successful");
+    const passwordValue = password.value.trim();
 
-        localStorage.setItem("isVerified", "true");
-
-        window.location.href = "homepage.html";
-    } else {
-        console.log("Verification Unsuccessful");
-        error("Wrong Password!");
-    }
+    fetch('http://localhost:3000/password-validation', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            password: passwordValue
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(text => {
+                alert("Wrong Password!");
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.success) {
+            localStorage.setItem("isVerified", "true");
+            window.location.href = "homepage.html";
+        } else {
+            alert("Invalid Email, Username or Password!");
+        }
+    })
+    .catch(err => console.error("Error: ", err));
 }
 
 function error(message) {
